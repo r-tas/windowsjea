3593e66f8edd21bf71121689d9c2b218602cf1d5
r-tas <github@undefined.pl>
2020-12-08 16:19:30 +0100

    added type for sessionconfiguration provider

---
+++ lib/puppet/type/sessionconfiguration.rb
@@ -0,0 +1,21 @@
+Puppet::Type.newtype(:sessionconfiguration) do
+  @doc = <<-'EOT'
+  Manage Powershell Session Configurations using .pssc files
+  EOT
+
+  ensurable do
+    defaultvalues
+    defaultto :absent
+  end
+
+  newparam(:name, :namevar => true) do
+    desc 'Configuration name'
+  end
+  newproperty(:configpath) do
+    desc 'Configuration file path'
+    validate do |value|
+      fail("Invalid path specified") unless value =~
+        /^(\w:\\.+$)/
+    end
+  end
+end
